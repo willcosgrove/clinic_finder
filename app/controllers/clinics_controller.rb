54deace610e722fef8fc66c0d93c79e6d9171af8
Will Cosgrove <will@willcosgrove.com>
2015-06-14 21:07:59 -0500

    First pass of search API

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -1,5 +1,20 @@
 class ClinicsController < ApplicationController
   def index
-    render json: []
+    if params[:zip_code]
+      geo = Geokit::Geocoders::MultiGeocoder.geocode(params[:zip_code])
+      if geo.success?
+        search = Clinic.search "*",
+          where: {location: {near: [geo.lat, geo.lng], within: '50mi'}},
+          boost_by_distance: {field: :location, origin: [geo.lat, geo.lng]},
+          limit: 10,
+          load: false
+        @results = search.results.map { |result| result.slice(:name, :address_1, :address_2, :address_3, :city, :state, :zip_code, :phone, :fax) }
+      else
+        @results = []
+      end
+    else
+      @results = []
+    end
+    render json: @results
   end
 end
--- test/controllers/clinics_controller_test.rb
+++ test/controllers/clinics_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class ClinicsControllerTest < ActionController::TestCase
+  before(:all) do
+    Clinic.reindex
+  end
+
   it "will reply :unauthorized if there is no X-Auth-Token header" do
     get :index
     assert_response :unauthorized
@@ -10,5 +14,17 @@ class ClinicsControllerTest < ActionController::TestCase
     @request.headers['X-Auth-Token'] = ENV['API_TOKEN']
     get :index
     assert_response :success
+  end
+
+  it "returns an empty array when no zip code is provided" do
+    @request.headers['X-Auth-Token'] = ENV['API_TOKEN']
+    get :index
+    assert_empty(assigns(:results))
+  end
+
+  it "returns results when a zip code is provided" do
+    @request.headers['X-Auth-Token'] = ENV['API_TOKEN']
+    get :index, zip_code: '76134'
+    refute_empty(assigns(:results))
   end
 end
